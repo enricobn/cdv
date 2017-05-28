515eacc3b3290dcf4857cd6bb47c3c4924df53f1
Enrico Benedetti <enrico_benedetti@yahoo.com>
2017-05-28 13:52:46 +0200

    Handled package rename.

--- intellij/src/main/java/org/cdv/intellij/core/CDVRefactoringElementListenerProvider.java
+++ intellij/src/main/java/org/cdv/intellij/core/CDVRefactoringElementListenerProvider.java
@@ -7,6 +7,7 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiPackage;
 import com.intellij.psi.search.FilenameIndex;
 import com.intellij.refactoring.listeners.RefactoringElementAdapter;
 import com.intellij.refactoring.listeners.RefactoringElementListener;
@@ -32,6 +33,8 @@ public class CDVRefactoringElementListenerProvider implements RefactoringElement
         if (oldPsiElement instanceof PsiClass) {
             PsiClass psiClass = (PsiClass) oldPsiElement;
             oldName = psiClass.getQualifiedName();
+        } else if (oldPsiElement instanceof PsiPackage) {
+            oldName = ((PsiPackage) oldPsiElement).getQualifiedName();
         } else {
             oldName = null;
         }
@@ -46,22 +49,23 @@ public class CDVRefactoringElementListenerProvider implements RefactoringElement
             protected void elementRenamedOrMoved(@NotNull final PsiElement psiElement) {
                 LOGGER.info("Moved or renamed " + oldName + " to " + psiElement);
 
-                final String newName;
-
-                if (psiElement instanceof PsiClass) {
-                    newName = ((PsiClass) psiElement).getQualifiedName();
-                } else {
-                    newName = null;
-                }
-
-                if (oldName != null && newName != null) {
+                if (oldName != null) {
                     for (final VirtualFile cdv : FilenameIndex.getAllFilesByExt(psiElement.getProject(), "cdv")) {
-                        try {
-                            renameClass(psiElement.getProject(), oldName, newName, cdv);
-                        } catch (Exception e) {
-                            e.printStackTrace();
+                        if (psiElement instanceof PsiClass) {
+                            String newName = ((PsiClass) psiElement).getQualifiedName();
+                            try {
+                                renameClass(psiElement.getProject(), oldName, newName, cdv);
+                            } catch (Exception e) {
+                                LOGGER.info(e);
+                            }
+                        } else if (psiElement instanceof PsiPackage) {
+                            String newName = ((PsiPackage) psiElement).getQualifiedName();
+                            try {
+                                renamePackage(psiElement.getProject(), oldName, newName, cdv);
+                            } catch (Exception e) {
+                                LOGGER.info(e);
+                            }
                         }
-                        LOGGER.info(cdv.toString());
                     }
                 }
             }
@@ -69,34 +73,54 @@ public class CDVRefactoringElementListenerProvider implements RefactoringElement
         };
     }
 
+    private void renamePackage(Project project, String oldName, String newName, VirtualFile cdv) throws Exception {
+        final CDVGraph cdvGraph = reader.read(cdv.getInputStream());
+        for (final CDVModule cdvModule : cdvGraph.getModules()) {
+            if (cdvModule.getFullName().startsWith(oldName + ".")) {
+                final CDVGraph newGraph = replaceModule(cdvGraph, cdvModule,
+                        newName + cdvModule.getFullName().substring(oldName.length()));
+                saveGraph(project, cdv, newGraph);
+            }
+        }
+    }
+
     private void renameClass(final Project project, String oldName, final String newName, final VirtualFile cdv) throws Exception {
         final CDVGraph cdvGraph = reader.read(cdv.getInputStream());
         for (final CDVModule cdvModule : cdvGraph.getModules()) {
             if (cdvModule.getFullName().equals(oldName)) {
-                final CDVGraph newGraph = new CDVGraph() {
-                    @Override
-                    public Set<CDVModule> getModules() {
-                        Set<CDVModule> modules = new HashSet<>(cdvGraph.getModules());
-                        modules.remove(cdvModule);
-                        modules.add(new CDVModuleImpl(newName));
-                        return modules;
-                    }
-                };
+                final CDVGraph newGraph = replaceModule(cdvGraph, cdvModule, newName);
 
-                CDVIDEAUtils.runUndoableWriteActionCommand(project, new Runnable() {
-                    @Override
-                    public void run() {
-                        try {
-                            Document document = FileDocumentManager.getInstance().getDocument(cdv);
-                            String text = CDVGraphUtils.toString(newGraph);
-                            document.setText(text);
-                        } catch (Exception e) {
-                            throw new RuntimeException(e);
-                        }
-                    }
-                }, "save", CDVRefactoringElementListenerProvider.this);
+                saveGraph(project, cdv, newGraph);
             }
         }
+    }
+
+    @NotNull
+    private static CDVGraph replaceModule(final CDVGraph cdvGraph, final CDVModule moduleToReplace, final String newFullName) {
+        return new CDVGraph() {
+                        @Override
+                        public Set<CDVModule> getModules() {
+                            Set<CDVModule> modules = new HashSet<>(cdvGraph.getModules());
+                            modules.remove(moduleToReplace);
+                            modules.add(new CDVModuleImpl(newFullName));
+                            return modules;
+                        }
+                    };
+    }
+
+    private static void saveGraph(Project project, final VirtualFile cdv, final CDVGraph newGraph) {
+        CDVIDEAUtils.runUndoableWriteActionCommand(project, new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Document document = FileDocumentManager.getInstance().getDocument(cdv);
+                    String text = CDVGraphUtils.toString(newGraph);
+                    document.setText(text);
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }, "save", null);
     }
 
 
